72f467f1d7a089d57e76369c4a7c7ccc4eb469fb
GustavoArcoverde <garcoverde3@gmail.com>
2022-04-13 13:12:00 -0300

    feat mostrar conteudo baseado na data escolhida

--- index.js
+++ index.js
@@ -6,14 +6,23 @@ $('#submit').click(function(event){
 function mostraImagem() {
     const keyPessoal = 'kwfP9ISXQLFnixhVJoHZG8PMC0OMncBUFTEcsSYJ';
     const data = $('#data').val()
+    const nomeDaFoto = $('#nomeDaFoto')
+    const imagem = $('#imagem')
+    const video = $('#video')
 
     $.ajax({
         url: `https://api.nasa.gov/planetary/apod?api_key=${keyPessoal}&date=${data}`,
         success: function (resposta) {
             if (resposta.media_type == 'image') {
-                console.log("Oi mundo")
-            } else if (resposta.media_type == 'video') {
-                console.log("Tchau mundo")
+                imagem.show();
+                video.hide()
+                nomeDaFoto.text(resposta.title)
+                imagem.attr('src', resposta.url);
+            } else {
+                imagem.hide();
+                video.show();
+                nomeDaFoto.text(resposta.title);
+                video.attr('src', resposta.url);
             }
         }
     })
