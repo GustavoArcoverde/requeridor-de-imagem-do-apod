8452a34efae3eaf37bd729c0e260eeed3dcb9ed4
GustavoArcoverde <garcoverde3@gmail.com>
2022-04-13 14:59:09 -0300

    add desc da img e corrige posição do conteudo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-$('#submit').click(function(event){
+$('#submit').click(function (event) {
     event.preventDefault('')
     mostraImagem()
 })
@@ -9,21 +9,27 @@ function mostraImagem() {
     const nomeDaFoto = $('#nomeDaFoto')
     const imagem = $('#imagem')
     const video = $('#video')
+    const descricao = $('#descricao')
 
     $.ajax({
         url: `https://api.nasa.gov/planetary/apod?api_key=${keyPessoal}&date=${data}`,
         success: function (resposta) {
-            if (resposta.media_type == 'image') {
-                imagem.show();
-                video.hide()
-                nomeDaFoto.text(resposta.title)
-                imagem.attr('src', resposta.url);
-            } else {
-                imagem.hide();
-                video.show();
-                nomeDaFoto.text(resposta.title);
-                video.attr('src', resposta.url);
-            }
+                if (resposta.media_type == 'image') {
+                    imagem.show();
+                    video.hide();
+                    $('#tituloEDescricao').css('visibility', 'visible')
+                    nomeDaFoto.text(resposta.title)
+                    descricao.text(resposta.explanation);
+                    imagem.attr('src', resposta.url);
+                } else {
+                    imagem.hide();
+                    video.show();
+                    $('#tituloEDescricao').css('visibility', 'visible')
+                    nomeDaFoto.text(resposta.title)
+                    nomeDaFoto.text(resposta.title);
+                    descricao.text(resposta.explanation);
+                    video.attr('src', resposta.url);
+                }
         }
     })
 }
